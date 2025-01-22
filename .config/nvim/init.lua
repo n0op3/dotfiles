a9663d2496707ed373e35a39e3fcf666e34b97e7
Shizukana Otaku <otakushizukana@gmail.com>
2025-01-22 17:01:39 +0000

    nvim fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,22 +6,22 @@ require("mason-lspconfig").setup()
 require("config.lspconfig")
 require("config.cmp")
 require("conform").setup {
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 }
 require("todo-comments").setup()
 require("lualine").setup {
-  options = { theme = "ayu_mirage" }
+	options = { theme = "ayu_mirage" }
 }
 
 local ccc = require("ccc")
 ccc.setup({
-  highlighter = {
-    auto_enable = true,
-    lsp = true,
-  },
+	highlighter = {
+		auto_enable = true,
+		lsp = true,
+	},
 })
 
 vim.notify = require("notify")
