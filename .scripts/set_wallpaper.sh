fe08e851c0ddc1247b45cccaaeaf469268f96a22
Shizukana Otaku <otakushizukana@gmail.com>
2025-01-30 17:51:51 +0000

    Break down set_wallpaper.sh

--- .scripts/set_wallpaper.sh
+++ .scripts/set_wallpaper.sh
@@ -5,20 +5,3 @@ swww img $1 --transition-type center --transition-duration 0.25 --transition-fps
 
 cp $1 ~/.cache/current_wallpaper.png
 cp $1 ~/.config/rofi/background.png
-
-rm ~/.cache/wal
-wal -i ~/.cache/current_wallpaper.png --saturate 0.9 --contrast 1.5
-
-walcord # Vesktop theme update
-pywalfox update # Refresh firefox theme
-
-# Bar
-hyprpanel -q 
-start-stop-daemon hyprpanel &
-
-# App launcher
-killall ulauncher 
-ln -sf ~/.cache/wal/template.css ~/.config/ulauncher/user-themes/pywal-ulauncher-theme
-start-stop-daemon ulauncher &
-
-exit
