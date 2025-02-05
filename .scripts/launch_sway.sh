669f471b0bdf0891f53049155612ea934d224384
Shizukana Otaku <otakushizukana@gmail.com>
2025-02-05 19:20:23 +0000

    Sway "rice" ig?

--- .scripts/launch_sway.sh
+++ .scripts/launch_sway.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export GDK_BACKEND=wayland
-export QT_QPA_PLATFORMTHEME=qt5ct #env = QT_STYLE_OVERRIDE,kvantum
+export QT_QPA_PLATFORMTHEME=qt5ct
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_QPA_PLATFORM=wayland
