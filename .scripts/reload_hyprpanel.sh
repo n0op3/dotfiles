2842643a691ae10304c10462ca147f7b595bad02
ShizukanaOtaku <otakushizukana@gmail.com>
2025-02-21 21:23:59 +0100

    Fix yazi wallpaper setting, start-stop-daemon scripts

--- .scripts/reload_hyprpanel.sh
+++ .scripts/reload_hyprpanel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hyprpanel -q 
-start-stop-daemon hyprpanel &
+start-stop-daemon --start hyprpanel &
--- .scripts/reload_ulauncher.sh
+++ .scripts/reload_ulauncher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 killall ulauncher 
 ln -sf ~/.cache/wal/template.css ~/.config/ulauncher/user-themes/pywal-ulauncher-theme
-start-stop-daemon ulauncher &
+start-stop-daemon --start ulauncher &
