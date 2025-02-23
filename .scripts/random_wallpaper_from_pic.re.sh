d42617dc90f52765d1f9c146b1793315840af2bb
ShizukanaOtaku <otakushizukana@gmail.com>
2025-02-23 16:10:19 +0100

    Change artix to arch again

--- .scripts/random_wallpaper_from_pic.re.sh
+++ .scripts/random_wallpaper_from_pic.re.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set_wallpaper.sh $wallpapers_path/artix/$(ls $wallpapers_path/artix | shuf -n 1)
+set_wallpaper.sh $wallpapers_path/arch/$(ls $wallpapers_path/arch | shuf -n 1)
 curl --location https://pic.re/image -o $wallpapers_path/random_wallpaper.png
 set_wallpaper.sh $wallpapers_path/random_wallpaper.png
 # convert $wallpapers_path/random_wallpaper.png $wallpapers_path/random_wallpaper.png # Ensure png format
