f5dca6343124edc1186ad0e196acba6fd7ccaa8c
ShizukanaOtaku <otakushizukana@gmail.com>
2025-02-24 18:19:28 +0100

    Add a failure case for pywal generation

--- .scripts/regen_pywal.sh
+++ .scripts/regen_pywal.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-wal -i ~/.cache/current_wallpaper.png --saturate 0.9 --contrast 1.5
+if ! wal -i ~/.cache/current_wallpaper.png --saturate 0.9 --contrast 1.5; then
+    notify-send "Failed to generate color scheme"
+    exit 1
+fi
 
 walcord # Vesktop theme update
 pywalfox update # Refresh firefox theme
