ec1100c70fcd0d63f62860291f2ab131bf21a6bd
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-17 11:59:00 +0100

    Set swww filter to nearest for gifs

--- .scripts/set_wallpaper.sh
+++ .scripts/set_wallpaper.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # ps aux | grep 'set_wallpaper.sh' | grep -v $$ | awk '{print $2}' | xargs kill # Kill other set_wallpapers
 
-swww img $1 --transition-type center --transition-duration 0.25 --transition-fps 30
+filter=""
+if identify ~/.cache/current_wallpaper | grep GIF; then
+    filter="--filter Nearest"
+fi
+
+swww img $1 --transition-type center --transition-duration 0.25 --transition-fps 30 $filter
 
 cp $1 ~/.cache/current_wallpaper.png
 cp $1 ~/.cache/current_wallpaper.jpg
