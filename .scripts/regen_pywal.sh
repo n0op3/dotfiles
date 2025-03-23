24d94ea70750ffdc2bed09a6079110f9022f27b1
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-23 12:25:33 +0100

    minor wal tweaks

--- .scripts/regen_pywal.sh
+++ .scripts/regen_pywal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! wal -i ~/.cache/current_wallpaper.png --saturate 0.9 --contrast 0; then
+if ! wal -i ~/.cache/current_wallpaper.png --saturate 1 --contrast -3; then
     notify-send "Failed to generate color scheme"
     exit 1
 fi
