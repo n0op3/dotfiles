8327cf01d9a61fc55ff9af4933851b1cd60072c5
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-04-27 21:44:52 +0200

    hyprland tweaks, regen pywal with yazi

--- .scripts/cht.sh
+++ .scripts/cht.sh
@@ -5,9 +5,4 @@ subject=$(echo $subject | tr ' ' '/')
 
 result=$(curl -s cht.sh/$subject)
 
-if [[ "$result" = *"<title>500 Internal Server Error</title>"* ]]; then
-  printf "\033[0;91mSorry, nothing was found.\033[0m\n" | less -R
-  exit
-else
-  printf "%s\n" "$result" | less -R
-fi
+printf "%s\n" "$result" | less -R
