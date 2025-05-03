9e5a62522499e13397e0df09a790a59645eb14e3
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-03 13:09:50 +0200

    fix wallpapers and hibernation

--- .scripts/set_wallpaper.sh
+++ .scripts/set_wallpaper.sh
@@ -6,7 +6,7 @@ if identify ~/.cache/current_wallpaper | grep GIF; then
     filter="--filter Nearest"
 fi
 
-swww img $1 --transition-type center --transition-duration 0.25 --transition-fps 30 $filter
+swww img "$1" --transition-type center --transition-duration 0.25 --transition-fps 30 $filter
 
 cp $1 ~/.cache/current_wallpaper.png
 cp $1 ~/.cache/current_wallpaper.jpg
