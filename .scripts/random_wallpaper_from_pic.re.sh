f2b2809ac2ebfc8077bc83aebb54d69fcbcd2ecd
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-10 16:23:07 +0200

    find files only for random wallpapers

--- .scripts/random_wallpaper_from_pic.re.sh
+++ .scripts/random_wallpaper_from_pic.re.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-set_wallpaper.sh $wallpapers_path/arch/$(ls $wallpapers_path/arch | shuf -n 1)
+set_wallpaper.sh $wallpapers_path/arch/$(find $wallpapers_path -type f | shuf -n 1)
 curl --location https://pic.re/image -o ~/.cache/random_wallpaper.png
 set_wallpaper.sh ~/.cache/random_wallpaper.png
