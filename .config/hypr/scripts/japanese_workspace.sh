d94a0e2820de83dc4536d4b167e0dd75af32c891
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-17 20:33:40 +0200

    add translators to japanese study

--- .config/hypr/scripts/japanese_workspace.sh
+++ .config/hypr/scripts/japanese_workspace.sh
@@ -3,4 +3,6 @@ hyprctl dispatch focusworkspaceoncurrentmonitor 10
 sleep 1; anki &
 sleep 1; exec "$1" \
     https://jisho.org \
+    https://deepl.com \
+    https://translate.google.com \
     https://kanji.sljfaq.org &
