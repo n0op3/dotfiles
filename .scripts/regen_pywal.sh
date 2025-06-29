39569eabdf506f8ef6ef9518512138702cd2e8cc
n0op3 <otakushizukana@gmail.com>
2025-06-29 21:49:24 +0200

    smart pywal color switching in zsh

--- .scripts/regen_pywal.sh
+++ .scripts/regen_pywal.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -f ~/.cache/pywal_on ]; then
+    exit 1
+fi
+
 if ! wal -i ~/.cache/current_wallpaper.png --saturate 1 --contrast -3; then
     notify-send "Failed to generate color scheme"
     exit 1
