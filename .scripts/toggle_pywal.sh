d4be889092094503b3b0efbeb0f69045b99fc55f
n0op3 <otakushizukana@gmail.com>
2025-06-29 21:33:02 +0200

    toggle pywal settings

--- .scripts/toggle_pywal.sh
+++ .scripts/toggle_pywal.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 if [ -f "$HOME/.cache/pywal_on" ]; then
     rm ~/.cache/pywal_on
-    rm -rf ~/.cache/wal
+    rm -rf ~/.cache/wal/colors-hyprland.conf
+           ~/.cache/wal/colors-alacritty.toml
+           ~/.cache/wal/colors-kitty.conf
+    mkdir -p ~/.cache/wal
+    echo "\$color1 = 0xFF4BE875
+          \$color4 = 0xFF85E0F4" > ~/.cache/wal/colors-hyprland.conf
 else
     regen_pywal.sh
     touch ~/.cache/pywal_on
