0a923439f8b393e09b18f5ea292a0c05179d9d2d
n0op3 <otakushizukana@gmail.com>
2025-06-30 12:13:30 +0200

    fix pywal_toggle

--- .scripts/toggle_pywal.sh
+++ .scripts/toggle_pywal.sh
@@ -7,9 +7,11 @@ if [ -f "$HOME/.cache/pywal_on" ]; then
     mkdir -p ~/.cache/wal
     echo "\$color1 = 0xFF4BE875
           \$color4 = 0xFF85E0F4" > ~/.cache/wal/colors-hyprland.conf
+    notify-send "Pywal off"
 else
     ~/.scripts/regen_pywal.sh
     touch ~/.cache/pywal_on
+    notify-send "Pywal on"
 fi
 
 hyprctl reload
