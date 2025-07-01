e58e538da199dcd9f2336985f433675c3c017274
n0op3 <otakushizukana@gmail.com>
2025-07-01 21:28:38 +0200

    generalize hypr packages search in backup

--- .scripts/update_with_backup.sh
+++ .scripts/update_with_backup.sh
@@ -5,7 +5,7 @@ backup() {
 }
 
 echo "Checking whether a backup is advised..."
-packages=$(paru -Quq | grep -i -E "linux|nvidia|hyprland|hyprutils|hyprlang|aquamarine|sway")
+packages=$(paru -Quq | grep -i -E "linux|nvidia|hypr|aquamarine|sway")
 backup_needed=$?
 
 if [[ $backup_needed ]]; then
