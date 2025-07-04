4f9a35a2ac60a3b7d695bee523692ed6da8d6ad0
n0op3 <otakushizukana@gmail.com>
2025-07-04 11:30:28 +0200

    more wal fixes

--- .scripts/regen_pywal.sh
+++ .scripts/regen_pywal.sh
@@ -3,7 +3,7 @@ if [ ! -f ~/.cache/pywal_on ]; then
     exit 1
 fi
 
-if ! wal -i ~/.cache/current_wallpaper.png; then
+if ! wal -i ~/.cache/current_wallpaper.png --saturate 0.4 --contrast 5; then
     notify-send "Failed to generate color scheme"
     exit 1
 fi
