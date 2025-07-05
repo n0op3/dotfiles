a1679a8f1f40618d184deeb07d706ae1e4c3692f
n0op3 <otakushizukana@gmail.com>
2025-07-05 21:51:36 +0200

    setup script

---
+++ .scripts/setup_env.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+install_toolchains() {
+    if ! type paru > /dev/null; then
+        echo "Paru is not installed, cloning..."
+        git clone https://aur.archlinux.org/paru /tmp/paru
+        cd /tmp/paru
+
+        if ! makepkg -si; then
+            echo "Installation failed, aborting!"
+            echo "Cleaning up..."
+            cd -
+            rm -rf /tmp/paru
+            exit 1
+        fi
+
+        echo "Cleaning up..."
+        cd -
+        rm -rf /tmp/paru
+    fi
+
+    echo "Paru is installed, proceeding..."
+
+    paru -S --needed --noconfirm rustup
+    rustup default stable
+}
+
+install_deps() {
+    # Utilities
+    paru -S --needed --noconfirm timeshift man-db tldr ufw
+    # Desktop Environment
+    paru -S --needed --noconfirm hyprland hyprlock hypridle uwsm waybar brigthnessctl rofi-wayland fcitx5 fcitx5-configtool fcitx5-mozc kitty mpv playerctl iwgtk clipse zen-browser-bin
+    # Theming
+    paru -S --needed --noconfirm python-pywal16 swww walcord
+    # Shell
+    paru -S --needed --noconfirm neovim starship oh-my-posh-bin tmux yazi zsh fastfetch fzf zinit fd git zoxide
+}
+
+setup() {
+    sudo usermod -aG games power docker video audio $USER
+    sudo usermod -s /bin/zsh $USER
+    sudo ufw enable
+}
+
+install_toolchains
+install_deps
+setup
+
+echo "Configuration finished, please reboot"
