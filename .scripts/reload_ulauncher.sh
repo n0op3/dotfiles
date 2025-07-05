a678cc1009bed82e53e2458d3d72e8894caffe56
n0op3 <otakushizukana@gmail.com>
2025-07-05 21:31:09 +0200

    rm old files from hypr

--- .scripts/reload_ulauncher.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-killall ulauncher 
-ln -sf ~/.cache/wal/template.css ~/.config/ulauncher/user-themes/pywal-ulauncher-theme
-start-stop-daemon --start ulauncher &
--- .scripts/restart_hyprlock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-killall hyprlock
-hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1'
-hyprctl --instance 0 'dispatch exec hyprlock'
