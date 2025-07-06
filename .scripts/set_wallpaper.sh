cb29318107ffcecce20bdba4e0a183fefcbe4d42
n0op3 <otakushizukana@gmail.com>
2025-07-06 10:55:47 +0200

    convert wallpapers to 2 formats

--- .scripts/set_wallpaper.sh
+++ .scripts/set_wallpaper.sh
@@ -8,8 +8,8 @@ fi
 
 swww img "$1" --transition-type center --transition-duration 0.25 --transition-fps 30 $filter
 
-cp $1 ~/.cache/current_wallpaper.png
-cp $1 ~/.cache/current_wallpaper.jpg
-cp $1 ~/.cache/current_wallpaper
+convert $1 ~/.cache/current_wallpaper.png
+convert $1 ~/.cache/current_wallpaper.jpg
+cp ~/.cache/current_wallpaper.png ~/.cache/current_wallpaper
 rm ~/.config/rofi/background.png
-magick ~/.cache/current_wallpaper.png -resize 1000x400 ~/.config/rofi/background.png # to speed up rofi image loading times
+magick ~/.cache/current_wallpaper.png -resize 1000x400 ~/.config/rofi/background.png # to speed up rofi image loading times if the wallpaper is used in the theme
