92c12fc3cb6c09121f838b91e66a89856a540448
n0op3 <n0op3@proton.me>
2025-08-30 16:55:37 +0200

    rewrite the monitors to the v2 syntax

--- .scripts/regen_pywal.sh
+++ .scripts/regen_pywal.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ ! -f ~/.pywal_on ]; then
-    exit 1
-fi
-
 if ! wal -i ~/.cache/current_wallpaper.png --saturate 0.7 --contrast 8; then
     notify-send "Failed to generate color scheme"
     exit 1
