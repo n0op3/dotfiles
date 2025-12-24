3f614da2d1661d2b2e20d3076356613b19baacf7
n0op3 <n0op3@proton.me>
2025-12-24 13:47:52 +0100

    why the reload bruv

--- .scripts/toggle_wallpaper.sh
+++ .scripts/toggle_wallpaper.sh
@@ -5,7 +5,6 @@ if [ $? -eq 0 ]; then
     hyprctl keyword decoration:active_opacity 1
     hyprctl keyword decoration:inactive_opacity 1
 else
-    hyprctl reload
     swww-daemon &
     set_wallpaper.sh ~/.cache/current_wallpaper.png
 fi
