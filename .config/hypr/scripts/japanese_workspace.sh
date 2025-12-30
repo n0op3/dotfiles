4d81f30cd24107c8d81f5d275ee84cfdf83dc937
n0op3 <n0op3@proton.me>
2025-12-30 15:11:31 +0100

    rm japanese workspace

--- .config/hypr/scripts/japanese_workspace.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-hyprctl dispatch focusworkspaceoncurrentmonitor 10
-sleep 1; anki &
-sleep 1; exec "$1" \
-    https://jisho.org \
-    https://deepl.com \
-    https://translate.google.com \
-    https://ichi.moe \
-    https://kanji.sljfaq.org &
