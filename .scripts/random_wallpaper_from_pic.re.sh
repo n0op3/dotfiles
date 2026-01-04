09076328af19081430cb319426ea7311c54b3ca6
n0op3 <n0op3@proton.me>
2026-01-04 20:05:52 +0100

    toggle the wallpaper when getting a random one

--- .scripts/random_wallpaper_from_pic.re.sh
+++ .scripts/random_wallpaper_from_pic.re.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-set_wallpaper.sh $wallpapers_path/arch/$(find $wallpapers_path -type f | shuf -n 1)
+swww kill
 curl --location https://pic.re/image -o ~/.cache/random_wallpaper.png
 set_wallpaper.sh ~/.cache/random_wallpaper.png
+swww-daemon & disown
