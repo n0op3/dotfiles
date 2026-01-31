b7df84c842e811ada4ab7ac5eae0e2de815b5c4d
n0op3 <n0op3@proton.me>
2026-01-31 15:41:36 +0100

    wallpaper script fix

--- .scripts/random_wallpaper_from_pic.re.sh
+++ .scripts/random_wallpaper_from_pic.re.sh
@@ -1,4 +1,4 @@
 swww kill
 curl --location https://pic.re/image -o ~/.cache/random_wallpaper.png
-set_wallpaper.sh ~/.cache/random_wallpaper.png
 swww-daemon & disown
+set_wallpaper.sh ~/.cache/random_wallpaper.png
