786bf2668cfd9711ae081ee5d02b624c851a13e3
n0op3 <n0op3@proton.me>
2026-02-04 20:39:13 +0100

    rm cht.sh

--- .scripts/cht.sh
+++
@@ -1,7 +0,0 @@
-printf "\033[0;32mWhat do you want to find?\033[0m\n> "
-read subject
-subject=$(echo $subject | tr ' ' '/')
-
-result=$(curl -s cht.sh/$subject)
-
-printf "%s\n" "$result" | less -R
