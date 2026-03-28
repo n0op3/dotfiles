3878f1f7cfe2d3b5c8b2342fb2286ec7d004aef0
n0op3 <n0op3@proton.me>
2026-03-28 12:41:21 +0100

    feat(hypr): no rounding on powersave

--- .config/hypr/scripts/powersave.sh
+++ .config/hypr/scripts/powersave.sh
@@ -7,6 +7,7 @@ else
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
+        keyword decoration:rounding 0;\
         keyword decoration:blur:enabled 0;"
     touch $HOME/.cache/hyprland-powersave
     killall swww-daemon
