cc8654b3096d3910c4964af4560136ef7907169d
n0op3 <n0op3@proton.me>
2026-04-14 21:04:36 +0200

    fix(zsh): pywal

--- .scripts/toggle_pywal.sh
+++ .scripts/toggle_pywal.sh
@@ -4,8 +4,8 @@ if [ -f "$HOME/.pywal_on" ]; then
            ~/.cache/wal/colors-alacritty.toml
            ~/.cache/wal/colors-kitty.conf
     mkdir -p ~/.cache/wal
-    echo "\$color1 = 0xFF4BE875
-          \$color4 = 0xFF85E0F4" > ~/.cache/wal/colors-hyprland.conf
+    echo "\$color5 = 0xFF4BE875
+          \$color6 = 0xFF85E0F4" > ~/.cache/wal/colors-hyprland.conf
     notify-send "Pywal off"
 else
     touch ~/.pywal_on
