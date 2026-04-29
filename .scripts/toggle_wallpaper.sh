10123e244cd2c362dab0ace2e23483ab0d066e09
n0op3 <n0op3@proton.me>
2026-04-29 23:34:22 +0200

    feat(hypr): disable hyprlock wallpaper

--- .scripts/toggle_wallpaper.sh
+++ .scripts/toggle_wallpaper.sh
@@ -1,9 +1,12 @@
 pidof awww-daemon
+
 if [ $? -eq 0 ]; then
     awww kill
     hyprctl keyword decoration:active_opacity 1
     hyprctl keyword decoration:inactive_opacity 1
+    mv ~/.cache/current_wallpaper ~/.cache/current_wallpaper.disabled
 else
+    mv ~/.cache/current_wallpaper.disabled ~/.cache/current_wallpaper
     awww-daemon &
     hyprctl reload
 fi
