f825fd2953c497f5fc0cce830469406645e663c7
n0op3 <n0op3@proton.me>
2026-07-13 13:04:21 +0200

    fix: wallpaper toggling

--- .scripts/set_wallpaper.sh
+++ .scripts/set_wallpaper.sh
@@ -15,9 +15,16 @@ fi
 
 awww img "$1" --transition-type $transition $filter
 
-cp "$1" ~/.cache/current_wallpaper
-cp "$1" ~/.cache/current_wallpaper.png
-cp "$1" ~/.cache/current_wallpaper.jpg
+SUFFIX=""
+
+pidof awww-daemon
+if [ $? -eq 1 ]; then
+    SUFFIX=".disabled"
+fi
+ 
+cp "$1" ~/.cache/current_wallpaper$SUFFIX
+cp "$1" ~/.cache/current_wallpaper.png$SUFFIX
+cp "$1" ~/.cache/current_wallpaper.jpg$SUFFIX
 rm ~/.config/rofi/background.png
 magick ~/.cache/current_wallpaper.png -resize 1000x400 ~/.config/rofi/background.png # to speed up rofi image loading times if the wallpaper is used in the theme
 
--- .scripts/toggle_wallpaper.sh
+++ .scripts/toggle_wallpaper.sh
@@ -5,8 +5,12 @@ if [ $? -eq 0 ]; then
     hyprctl keyword decoration:active_opacity 1
     hyprctl keyword decoration:inactive_opacity 1
     mv ~/.cache/current_wallpaper ~/.cache/current_wallpaper.disabled
+    mv ~/.cache/current_wallpaper.png ~/.cache/current_wallpaper.png.disabled
+    mv ~/.cache/current_wallpaper.jpg ~/.cache/current_wallpaper.jpg.disabled
 else
     mv ~/.cache/current_wallpaper.disabled ~/.cache/current_wallpaper
+    mv ~/.cache/current_wallpaper.png.disabled ~/.cache/current_wallpaper.png
+    mv ~/.cache/current_wallpaper.jpg.disabled ~/.cache/current_wallpaper.jpg
     awww-daemon &
     hyprctl reload
 fi
