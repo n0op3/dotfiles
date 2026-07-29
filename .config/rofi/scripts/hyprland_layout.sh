bad2bcabb9d6ae9d0bbaf0c50ec3d5efec4eaebc
n0op3 <n0op3@proton.me>
2026-07-29 11:35:31 +0200

    hyprland layout reset bind

--- .config/rofi/scripts/hyprland_layout.sh
+++ .config/rofi/scripts/hyprland_layout.sh
@@ -10,8 +10,19 @@ if [[ "$STATE" == "main" ]]; then
     do
         echo -en "$layout\0info\x1f$layout\n"
     done
+        echo -en "reset all\0info\x1freset all\n"
 else
-    hyprctl eval "hl.workspace_rule({ workspace = tostring(hl.get_active_workspace().id), layout = \"$1\" })" > /dev/null
-    notify-send Hyprland "Layout changed to $1."
+    if [[ "$1" == "reset all" ]]; then
+        hyprctl eval \
+        "for _, workspace in ipairs(hl.get_workspaces()) do \
+            hl.workspace_rule({ workspace = tostring(workspace.id), layout = hl.get_config('general.layout') }) \
+        end" \
+        > /dev/null
+        notify-send Hyprland "Reset all layouts."
+    else
+        hyprctl eval "hl.workspace_rule({ workspace = tostring(hl.get_active_workspace().id), layout = \"$1\" })" > /dev/null
+        notify-send Hyprland "Layout changed to $1."
+    fi
+
     exit
 fi
