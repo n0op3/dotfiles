4e8b491f1f9a0de89859c7b5821fecd6c061d37a
n0op3 <n0op3@proton.me>
2026-08-24 19:07:34 +0200

    fym binary error

--- .scripts/cava_pywal.sh
+++ .scripts/cava_pywal.sh
@@ -3,10 +3,4 @@ color2=$( awk 'match($0, /color1=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/col
 
 sed -e "s/%%%%%%%/$color2/" -e "s/+++++++/$color1/" ~/.config/cava/config.colors > /tmp/cava_colors
 cat ~/.config/cava/config >> /tmp/cava_colors
-pid=$(pidof cava)
-
-if [ -z $1 ]; then
-  [ -z $pid ] && cava -p /tmp/cava_colors || kill -USR2 $pid
-else
-  [ -z $pid ] || kill -USR2 $pid
-fi
+cava -p /tmp/cava_colors
