367d91721beb1e71a198e8d450a26664c1775dba
n0op3 <n0op3@proton.me>
2026-09-13 19:50:00 +0200

    gtk pywal

--- .scripts/regen_pywal.sh
+++ .scripts/regen_pywal.sh
@@ -7,3 +7,7 @@ swaync-client --reload-css & disown
 themecord -p & disown
 pywalfox update & disown
 pkill .waybar-wrapped && waybar & disown
+
+cp ~/.cache/wal/colors-gtk.css ~/.config/gtk-4.0/gtk.css
+cp ~/.cache/wal/colors-gtk.css ~/.config/gtk-3.0/gtk.css
+cp ~/.cache/wal/colors-gtk.css ~/.config/gtk-2.0/gtk.css
